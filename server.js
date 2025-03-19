7f35a8e349c9082e4261c2dd04800110db455a9b
KhacThien88 <KTeightop1512@gmail.com>
2025-03-19 13:54:03 +0700

    Update ways to use secret manager

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ app.use(bodyParser.json());
 const secret_name = "db_mysql";
 
 const client = new SecretsManagerClient({
-  region: "ap-southeast-1",
+  region: process.env.AWS_REGION || "ap-southeast-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 });
 
 let dbConfig;
