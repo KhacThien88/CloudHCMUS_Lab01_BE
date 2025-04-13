16df07c8add23e1ec3c38c10300d312c8aaf2f07
KhacThien88 <KTeightop1512@gmail.com>
2025-04-13 10:28:13 +0700

    update key

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /home/ec2-user/node
-npm install
-pm2 stop all || true
-pm2 delete all || true
-pm2 start server.js --name my-app
-pm2 save
-pm2 startup
+sudo npm install
+sudo pm2 stop all || true
+sudo pm2 delete all || true
+sudo pm2 start server.js --name my-app
+sudo pm2 save
+sudo pm2 startup
 exit 0
