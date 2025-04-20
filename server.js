af6bb471f5f2dc09414d537091e3c385825234ba
KhacThien88 <KTeightop1512@gmail.com>
2025-04-20 16:50:20 +0700

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const secret_name = "db_secret_connect_115";
+const secret_name = "db_secret_connect_116";
 
 const client = new SecretsManagerClient({
   region: process.env.AWS_REGION || "ap-southeast-1",
